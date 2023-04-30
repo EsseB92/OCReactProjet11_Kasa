941a67f6815df4af12ddbbff679cd0946214107f
Bertrand Sudre <sudrebertrand@gmail.com>
2023-04-30 16:20:59 +0200

    style: Ajout d'une taille pour la bannière

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Banner = ({ image, alt, text }) => {
+const Banner = ({ image, alt, text, size }) => {
     return (
-        <section className="banner">
+        <section className={`banner ${size}`}>
             <img className="banner__image" src={image} alt={alt} />
             {text ? <h1 className="banner__text">{text}</h1> : null}
         </section>
